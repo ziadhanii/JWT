40ba7bee27c09929747836c654c88c23981cd333
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2024-10-16 09:01:40 +0300

    Seed Database With Needed Roles

---
+++ JWT/Migrations/20241015212110_SeedData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JWT.Migrations;
+
+public partial class SeedData : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.InsertData(
+            table: "AspNetRoles",
+            columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+            values: new object[] { Guid.NewGuid().ToString(), "User", "USER", Guid.NewGuid().ToString() }
+        );
+
+        migrationBuilder.InsertData(
+            table: "AspNetRoles",
+            columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+            values: new object[] { Guid.NewGuid().ToString(), "Admin", "ADMIN", Guid.NewGuid().ToString() }
+        );
+
+    }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DeleteData(
+            table: "AspNetRoles",
+            keyColumn: "Name",
+            keyValues: new object[] { "User", "Admin" }
+        );
+    }
+}
