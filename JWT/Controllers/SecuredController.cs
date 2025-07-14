dbf1875a9d4d8047a756b34f3c62bae4604ef90c
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-07-14 10:34:07 +0300

    Restrict access to Admin role in Secured Controller

---
+++ JWT/Controllers/SecuredController.cs
@@ -0,0 +1,16 @@
+﻿namespace JWT.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class SecuredController : BaseApiController
+{
+    [HttpGet]
+    public IActionResult GetData()
+    {
+
+        return Ok("Hello From Secured Controller");
+
+    }
+
+
+
+}
